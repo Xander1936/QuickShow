eb3f4e16ffcecef0be40ffe09e0d14844b5710c3
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-08-04 20:09:46 +0100

    Homepage: Hero

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 // Home page component. This is the landing page rendered at the '/' route.
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <HeroSection/>
+    </>
   )
 }
 
