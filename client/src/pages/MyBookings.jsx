1de885d7b553598b9007d556c4447536fee6be44
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-08-20 14:01:10 +0100

    Seat Layout and Booking Process

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../assets/assets'
+import BlurCircle from '../components/BlurCircle'
+import Loading from '../components/Loading'
 
 const MyBookings = () => {
-  return (
-    <div>
-      
+  const currency = import.meta.env.VITE_CURRENCY
+
+
+  const [bookings, setBookings] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getMyBookings = async () => {
+    setBookings(dummyBookingData)
+    setIsLoading(false)
+  }
+
+  useEffect(()=> {
+    getMyBookings()
+  },[])
+
+  return !isLoading ? (
+    <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]'>
+      <BlurCircle top="100px" left="100px" />
+      <div>
+        <BlurCircle bottom="0px" left="600px" />
+      </div>
+      <h1>My Bookings</h1>
     </div>
-  )
+  ) : <Loading />
 }
 
 export default MyBookings
