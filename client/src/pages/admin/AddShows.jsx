ffa59985245c5938e21c802b1603c657b00ed13b
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-09-08 11:00:35 +0100

    Frontend: Integrating Real Backend Data

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -5,6 +5,7 @@ import Title from '../../components/admin/Title';
 import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react'
 import { kConverter } from '../../lib/kConverter';
 import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 // Admin form for selecting a movie, price, and showtimes.
 const AddShows = () => {
@@ -17,6 +18,7 @@ const AddShows = () => {
   const [dateTimeSelection, setDatetimeSelection] = useState({});
   const [dateTimeInput, setDateTimeInput] = useState("");
   const [showPrice, setShowPrice] = useState("");
+  const [addingShow, setAddingShow] = useState(false);
 
   const fetchNowPlayingMovies = async () => {
     try {
@@ -59,6 +61,46 @@ const AddShows = () => {
       };
     });
   };
+
+  const handleSubmit = async () => {
+    setAddingShow(true)
+
+    try {
+      if(!selectedMovie || Object.keys(dateTimeSelection).length === 0 || !showPrice ){
+        toast.error('Missing required fields');
+        return;
+      }
+      const showsInput = Object.entries(dateTimeSelection).map(([date, times]) => ({date, times}))
+
+      const payload = {
+        movieId: selectedMovie,
+        showsInput,
+        showPrice: Number(showPrice)
+      }
+
+      // Let's make the API call 
+      const { data } = await axios.post(
+        '/api/show/add', 
+        payload, 
+        {headers: {Authorization: `Bearer ${await getToken()}`}}
+      )
+
+      if (data.success) {
+        toast.success(data.message)
+        setSelectedMovie(null)
+        setDatetimeSelection({})
+        setShowPrice("")
+        setDateTimeInput("")
+      }else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.error("Submission error", error.response?.data || error);
+      toast.error(error.response?.data?.message || 'An error occurred. Please try again.')
+    } finally {
+      setAddingShow(false)
+    }
+  }
 
   useEffect(() => {
     if(user) {
@@ -175,13 +217,17 @@ const AddShows = () => {
         </div>
       )}
 
-      <button className="bg-primary text*white px-8 py-2 mt-6 rounded 
-      hover:bg-primary/90 transition-all cursor-pointer ">
+      <button 
+        onClick={handleSubmit} disabled={addingShow}
+        className="bg-primary text-white px-8 py-2 mt-6 rounded 
+      hover:bg-primary/90 transition-all cursor-pointer "
+      >
         Add Show
       </button>
 
     </>
   ) : <Loading />
 }
+
 
 export default AddShows
